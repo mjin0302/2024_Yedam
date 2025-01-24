8bb6ed9438163dca8ca53a7db2c3fb0cbceeb50b
jin ◡̈ <alswls7887@gmail.com>
2025-01-24 17:53:21 +0900

    homework

---
+++ homework/homework.md
@@ -0,0 +1,14 @@
+## 1번 영화정보
+- 파일 : movie01.html, movieinfo.is  
+- 요구사항 
+movieinfo.is 파일에 선언된 movieRank 배열을 출력하고 영화코드를 클릭하면 오른쪽 상세정보에 출력
+<img src="01.png">
+
+## 2번 영화정보 API
+- 영화진흥위원회 키 발급받기  
+  https://www.kobis.or.kr/kobisopenapi/homepg/apiservice/searchServiceInfo.do  
+- 요구사항  
+  1. 1번문제에서 movieinfo.is 파일에 선언된 movieRank 배열 대신에 ajax 함수를 이용하여 데이터를 받아서 출력하도록 변경하세요
+  2. input 태그에 오늘 날짜가 보이게 하고 날짜 입력하고 조회버튼 클릭하면 해당 날짜의 박스오피스를 ajax로 조회하여 출력하세요.
+
+<img src="02.png">
---
+++ homework/movieinfo.js
@@ -0,0 +1,351 @@
+var movieRank = [
+  {
+  "rnum": "1",
+  "rank": "1",
+  "rankInten": "8",
+  "rankOldAndNew": "OLD",
+  "movieCd": "20242248",
+  "movieNm": "글래디에이터 Ⅱ",
+  "openDt": "2024-11-13",
+  "salesAmt": "696135340",
+  "salesShare": "44.8",
+  "salesInten": "676339340",
+  "salesChange": "3416.5",
+  "salesAcc": "748339340",
+  "audiCnt": "72026",
+  "audiInten": "70452",
+  "audiChange": "4476",
+  "audiAcc": "75744",
+  "scrnCnt": "1769",
+  "showCnt": "6252"
+  },
+  {
+  "rnum": "2",
+  "rank": "2",
+  "rankInten": "-1",
+  "rankOldAndNew": "OLD",
+  "movieCd": "20233261",
+  "movieNm": "청설",
+  "openDt": "2024-11-06",
+  "salesAmt": "300950670",
+  "salesShare": "19.4",
+  "salesInten": "90961370",
+  "salesChange": "43.3",
+  "salesAcc": "3032072750",
+  "audiCnt": "32632",
+  "audiInten": "9473",
+  "audiChange": "40.9",
+  "audiAcc": "318628",
+  "scrnCnt": "810",
+  "showCnt": "2503"
+  },
+  {
+  "rnum": "3",
+  "rank": "3",
+  "rankInten": "-1",
+  "rankOldAndNew": "OLD",
+  "movieCd": "20240941",
+  "movieNm": "베놈: 라스트 댄스",
+  "openDt": "2024-10-23",
+  "salesAmt": "131103950",
+  "salesShare": "8.4",
+  "salesInten": "4821590",
+  "salesChange": "3.8",
+  "salesAcc": "15923157868",
+  "audiCnt": "13842",
+  "audiInten": "933",
+  "audiChange": "7.2",
+  "audiAcc": "1622009",
+  "scrnCnt": "682",
+  "showCnt": "1818"
+  },
+  {
+  "rnum": "4",
+  "rank": "4",
+  "rankInten": "-1",
+  "rankOldAndNew": "OLD",
+  "movieCd": "20233057",
+  "movieNm": "아마존 활명수",
+  "openDt": "2024-10-30",
+  "salesAmt": "57477000",
+  "salesShare": "3.7",
+  "salesInten": "-28146820",
+  "salesChange": "-32.9",
+  "salesAcc": "4936085760",
+  "audiCnt": "6829",
+  "audiInten": "-3050",
+  "audiChange": "-30.9",
+  "audiAcc": "552789",
+  "scrnCnt": "493",
+  "showCnt": "732"
+  },
+  {
+  "rnum": "5",
+  "rank": "5",
+  "rankInten": "44",
+  "rankOldAndNew": "OLD",
+  "movieCd": "20241180",
+  "movieNm": "괜찮아, 앨리스",
+  "openDt": "2024-11-13",
+  "salesAmt": "57674200",
+  "salesShare": "3.7",
+  "salesInten": "56855200",
+  "salesChange": "6942",
+  "salesAcc": "99578200",
+  "audiCnt": "6429",
+  "audiInten": "6338",
+  "audiChange": "6964.8",
+  "audiAcc": "11085",
+  "scrnCnt": "99",
+  "showCnt": "107"
+  },
+  {
+  "rnum": "6",
+  "rank": "6",
+  "rankInten": "0",
+  "rankOldAndNew": "NEW",
+  "movieCd": "20179281",
+  "movieNm": "컨택트",
+  "openDt": "2017-02-02",
+  "salesAmt": "38292360",
+  "salesShare": "2.5",
+  "salesInten": "38292360",
+  "salesChange": "100",
+  "salesAcc": "5252843881",
+  "audiCnt": "4221",
+  "audiInten": "4221",
+  "audiChange": "100",
+  "audiAcc": "638371",
+  "scrnCnt": "78",
+  "showCnt": "155"
+  },
+  {
+  "rnum": "7",
+  "rank": "7",
+  "rankInten": "0",
+  "rankOldAndNew": "NEW",
+  "movieCd": "20240774",
+  "movieNm": "4월이 되면 그녀는",
+  "openDt": "2024-11-13",
+  "salesAmt": "21467100",
+  "salesShare": "1.4",
+  "salesInten": "21467100",
+  "salesChange": "100",
+  "salesAcc": "27798100",
+  "audiCnt": "3720",
+  "audiInten": "3720",
+  "audiChange": "100",
+  "audiAcc": "4376",
+  "scrnCnt": "122",
+  "showCnt": "255"
+  },
+  {
+  "rnum": "8",
+  "rank": "8",
+  "rankInten": "41",
+  "rankOldAndNew": "OLD",
+  "movieCd": "20243696",
+  "movieNm": "연소일기",
+  "openDt": "2024-11-13",
+  "salesAmt": "30380920",
+  "salesShare": "2.0",
+  "salesInten": "29561920",
+  "salesChange": "3609.5",
+  "salesAcc": "104990920",
+  "audiCnt": "3633",
+  "audiInten": "3542",
+  "audiChange": "3892.3",
+  "audiAcc": "11898",
+  "scrnCnt": "288",
+  "showCnt": "408"
+  },
+  {
+  "rnum": "9",
+  "rank": "9",
+  "rankInten": "0",
+  "rankOldAndNew": "NEW",
+  "movieCd": "20225184",
+  "movieNm": "히든페이스",
+  "openDt": "2024-11-20",
+  "salesAmt": "30420000",
+  "salesShare": "2.0",
+  "salesInten": "30420000",
+  "salesChange": "100",
+  "salesAcc": "30868000",
+  "audiCnt": "3380",
+  "audiInten": "3380",
+  "audiChange": "100",
+  "audiAcc": "3412",
+  "scrnCnt": "17",
+  "showCnt": "17"
+  },
+  {
+  "rnum": "10",
+  "rank": "10",
+  "rankInten": "11",
+  "rankOldAndNew": "OLD",
+  "movieCd": "20245399",
+  "movieNm": "4분 44초",
+  "openDt": "2024-11-01",
+  "salesAmt": "5760000",
+  "salesShare": "0.4",
+  "salesInten": "3740000",
+  "salesChange": "185.1",
+  "salesAcc": "172846000",
+  "audiCnt": "1440",
+  "audiInten": "935",
+  "audiChange": "185.1",
+  "audiAcc": "43218",
+  "scrnCnt": "167",
+  "showCnt": "240"
+  }
+  ]
+   
+
+let movieInfo = {"20242248":{
+    "movieCd": "20242248",
+    "movieNm": "글래디에이터 Ⅱ",
+    "movieNmEn": "GladiatorⅡ",
+    "movieNmOg": "",
+    "showTm": "147",
+    "prdtYear": "2024",
+    "openDt": "20241113",
+    "prdtStatNm": "개봉",
+    "typeNm": "장편",
+    "directors": [{
+      "peopleNm": "리들리 스콧",
+      "peopleNmEn": "Ridley Scott"
+    }],
+    "actors": [{
+        "peopleNm": "폴 메스칼",
+        "peopleNmEn": "Paul Mescal"
+      },
+      {
+        "peopleNm": "페드로 파스칼",
+        "peopleNmEn": "Pedro Pascal"
+      },
+      {
+        "peopleNm": "덴젤 워싱턴",
+        "peopleNmEn": "Denzel Washington "
+      },
+      {
+        "peopleNm": "코니 닐슨",
+        "peopleNmEn": "Connie Nielsen"
+      },
+      {
+        "peopleNm": "조셉 퀸",
+        "peopleNmEn": "Joseph Quinn"
+      }
+    ]
+  },
+ "20233261": {
+    "movieCd": "20233261",
+    "movieNm": "청설",
+    "movieNmEn": "Hear Me: Our Summer",
+    "movieNmOg": "",
+    "showTm": "108",
+    "prdtYear": "2024",
+    "openDt": "20241106",
+    "nations": [{
+      "nationNm": "한국"
+    }],
+    "genres": [{
+        "genreNm": "드라마"
+      },
+      {
+        "genreNm": "멜로/로맨스"
+      }
+    ],
+    "directors": [{
+      "peopleNm": "조선호",
+      "peopleNmEn": "JO Seon-ho"
+    }],
+    "actors": [{
+        "peopleNm": "홍경",
+        "peopleNmEn": "HONG Kyung",
+        "cast": "용준",
+        "castEn": "Yong-jun"
+      },
+      {
+        "peopleNm": "노윤서",
+        "peopleNmEn": "",
+        "cast": "여름",
+        "castEn": "Yeo-reum"
+      },
+      {
+        "peopleNm": "김민주",
+        "peopleNmEn": "KIM Min-ju",
+        "cast": "가을",
+        "castEn": ""
+      },
+      {
+        "peopleNm": "정용주",
+        "peopleNmEn": "",
+        "cast": "재진",
+        "castEn": ""
+      },
+      {
+        "peopleNm": "정혜영",
+        "peopleNmEn": "JEONG Hye-young",
+        "cast": "미정",
+        "castEn": ""
+      }
+    ]
+  },
+  "20240941":
+  {
+    "movieCd": "20240941",
+    "movieNm": "베놈: 라스트 댄스",
+    "movieNmEn": "Venom: The Last Dance",
+    "movieNmOg": "",
+    "showTm": "108",
+    "prdtYear": "2024",
+    "openDt": "20241023",
+    "prdtStatNm": "개봉",
+    "typeNm": "장편",
+    "nations": [
+    {
+    "nationNm": "미국"
+    }
+    ],
+    "genres": [
+    {
+    "genreNm": "액션"
+    },
+    {
+    "genreNm": "SF"
+    },
+    {
+    "genreNm": "스릴러"
+    }
+    ],
+    "directors": [
+    {
+    "peopleNm": "켈리 마르셀",
+    "peopleNmEn": "Kelly Marcel"
+    }
+    ],
+    "actors": [
+    {
+    "peopleNm": "톰 하디",
+    "peopleNmEn": "Tom Hardy",
+    },
+    {
+    "peopleNm": "치웨텔 에지오포",
+    "peopleNmEn": "Chiwetel Ejiofor",
+    },
+    {
+    "peopleNm": "주노 템플",
+    "peopleNmEn": "Juno Temple",
+    },
+    {
+    "peopleNm": "리스 이판",
+    "peopleNmEn": "Rhys Ifans",
+    },
+    {
+    "peopleNm": "페기 루",
+    "peopleNmEn": "Peggy Lu",
+    "cast": "",
+    "castEn": ""
+    }
+    ]}}
